4e5fb9426b39822c40afa1a72449f7c0e4703899
HuianYuan <452970841@163.com>
2023-03-15 18:50:35 +0800

    Update _242.java

--- src/likou/_242.java
+++ src/likou/_242.java
@@ -7,12 +7,13 @@ package likou;
  */
 public class _242 {
     public static void main(String[] args) {
-        String s="abcdesf";
-        String t="cdsfeab";
-        boolean res = isAnagram(s, t);
+        String s="abc";
+        String t="cba";
+        boolean res = isAnagram2(s, t);
         System.out.println(res);
     }
 
+    //大小写通用
     public static boolean isAnagram(String s, String t) {
 
         if(s.length()!=t.length()) return false;
@@ -30,5 +31,25 @@ public class _242 {
             }
         }
         return count==s.length();
+    }
+
+    //经包含小写字母
+    public static boolean isAnagram2(String s, String t) {
+        int[] record = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            record[s.charAt(i) - 'a']++;     // 并不需要记住字符a的ASCII，只要求出一个相对数值就可以了
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            record[t.charAt(i) - 'a']--;
+        }
+
+        for (int count: record) {
+            if (count != 0) {               // record数组如果有的元素不为零0，说明字符串s和t 一定是谁多了字符或者谁少了字符。
+                return false;
+            }
+        }
+        return true;                        // record数组所有元素都为零0，说明字符串s和t是字母异位词
     }
 }
