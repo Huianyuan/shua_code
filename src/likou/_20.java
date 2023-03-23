23bb77fe8e72ab8fdd789dc9e8b10cb9d336823f
HuianYuan <452970841@163.com>
2023-03-23 14:19:37 +0800

    Create _20.java

---
+++ src/likou/_20.java
@@ -0,0 +1,42 @@
+package likou;
+
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author :lhy
+ * @description :TODO
+ * @date :2023/03/23 上午 10:30
+ */
+public class _20 {
+    public static void main(String[] args) {
+        String s="{}}{";
+        boolean res = isValid(s);
+        System.out.println(res);
+
+    }
+
+    public static boolean isValid(String s) {
+        Deque<Character> deque = new LinkedList<>();
+        char ch;
+        for (int i = 0; i < s.length(); i++) {
+            ch = s.charAt(i);
+            //碰到左括号，就把相应的右括号入栈
+            if (ch == '(') {
+                deque.push(')');
+            }else if (ch == '{') {
+                deque.push('}');
+            }else if (ch == '[') {
+                deque.push(']');
+            } else if (deque.isEmpty() || deque.peek() != ch) {
+                return false;
+            }else {//如果是右括号判断是否和栈顶元素匹配
+                deque.pop();
+            }
+        }
+        //最后判断栈中元素是否匹配
+        return deque.isEmpty();
+    }
+}
