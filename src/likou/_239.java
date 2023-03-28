2517fdce647952720ae1afd14a888967c079c87b
HuianYuan <452970841@163.com>
2023-03-28 10:05:21 +0800

    Create _239.java

---
+++ src/likou/_239.java
@@ -0,0 +1,61 @@
+package likou;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @author :lhy
+ * @description :TODO
+ * @date :2023/03/28 上午 09:46
+ */
+public class _239 {
+    public static void main(String[] args) {
+        int[] nums={1,3,-1,-3,5,3,6,7};
+        int k=2;
+        int[] ints = maxSlidingWindow(nums, k);
+
+    }
+
+    public static int[] maxSlidingWindow( int[] nums,int k){
+        if(nums.length==1) return nums;
+
+        int[] res = new int[nums.length-k+1];
+        int num=0;
+
+        MyQueue myQueue=new MyQueue();
+
+        for(int i=0;i<k;i++){
+            myQueue.add(nums[i]);
+        }
+
+        res[num++]=myQueue.peek();
+        for(int i=k;i<nums.length;i++){
+            myQueue.poll(nums[i-k]);
+            myQueue.add(nums[i]);
+            res[num++]=myQueue.peek();
+        }
+        return res;
+    }
+
+
+}
+class MyQueue{
+    Deque<Integer> deque=new LinkedList<>();
+
+    void poll(int val){
+        if(!deque.isEmpty() && val==deque.peek()){
+            deque.poll();
+        }
+    }
+
+    void add(int val){
+        while(!deque.isEmpty() && val>deque.getLast()){
+            deque.removeLast();
+        }
+        deque.add(val);
+    }
+
+    int peek(){
+        return deque.peek();
+    }
+}
